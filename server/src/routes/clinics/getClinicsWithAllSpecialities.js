c7d8ff524b9eb2b41ee5b18e2cb7202ed88e58b9
Rachit Malik <rachitmalik1947@gmail.com>
2020-06-16 19:49:25 +0530

    added division query and changed schema generation sql script

--- server/src/routes/clinics/getClinicsWithAllSpecialities.js
+++ server/src/routes/clinics/getClinicsWithAllSpecialities.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 const sqlClient = require('../../utils/sqlClient');
 
-router.get("/getClinicDetailsInCity/:city", (req, res) => {
+router.get("/getClinicsWithAllSpecialities/:city", (req, res) => {
     const cityName = req.params.city; 
-    const query = 'select c.id from clinic c where not exists( '
-     +'select s1.name from specialty s1 ' + 
-    'where not exists(select d.id, d.clinic_id from doctor d, specializes s2 where d.id = s2.did and d.clinic_id = c.id and s1.name = s2.name'
-    + '));'
+    const query = 'select * from clinic c where not exists(select s1.name from specialty s1 where not exists(select d.id, d.clinic_id from doctor d, specializes s2, cliniclocation cl where d.id = s2.did and d.clinic_id = c.id and s1.name = s2.name and c.postal_code = cl.postal_code and cl.city=$1));'
     sqlClient.query(query, [cityName], 
         (err, qres) => {
              if(err){
